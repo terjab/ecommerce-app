92f56436715ac4c17da28217eda4c4ccd92ca66b
Tereza Jabornická <t.jabornicka@gmail.com>
2020-11-26 12:39:25 +0100

    Minor changes

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    clientId: '8pZs_OKbVH1Ef_UNxSTU5y4fluge-LsqD3yCgXOjfV8',
-    scope: 'market:1446',
-    apiUrl: 'https://the-teal-brand-48.commercelayer.io'
+  clientId: '8pZs_OKbVH1Ef_UNxSTU5y4fluge-LsqD3yCgXOjfV8',
+  scope: 'market:1446',
+  apiUrl: 'https://the-teal-brand-48.commercelayer.io',
 }
