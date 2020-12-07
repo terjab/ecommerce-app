3c605b561438dbb90b895940c7848124c7d72653
Tereza Jabornická <t.jabornicka@gmail.com>
2020-12-07 20:40:45 +0100

    Delete reportWebVitals, add react-redux

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
 
 const render = () => {
   ReactDOM.render(
@@ -18,8 +17,3 @@ if (module.hot) {
 }
 
 render()
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
