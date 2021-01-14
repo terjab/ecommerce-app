e44149db3230d356c99409dec25adfd85ede7d0d
Tereza Jabornická <t.jabornicka@gmail.com>
2021-01-14 22:29:50 +0100

    Revert "Revert "Sign up page, refactor api client, private route""
    This reverts commit 35da7646b3c424bd354ef09b8099920ed5309406.

---
+++ src/api/products/get-products.js
@@ -0,0 +1,8 @@
+/* eslint-disable camelcase */
+import { api } from '../api-client'
+import { formatProduct } from './utils/format-product'
+
+export const getProducts = async () => {
+  const { data, included } = await api(`/api/skus?include=prices`)
+  return data.map(product => formatProduct(product, included))
+}
