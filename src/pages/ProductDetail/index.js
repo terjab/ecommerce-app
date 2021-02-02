1327d0439a1589397be570e4008f27f19d5e4f6c
Tereza Jabornická <t.jabornicka@gmail.com>
2021-02-02 10:45:08 +0100

    Product detail to hooks

--- src/pages/ProductDetail/index.js
+++ src/pages/ProductDetail/index.js
@@ -1,7 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+
 import { getProductById } from '../../api/products/get-product'
+import { useApi } from '../../api/use-api'
+import { addProduct as addProductAction } from '../../store/cart/actions'
+
 import { H1 } from '../../components/Typography'
 import Layout from '../../components/Layout'
 import Button from '../../components/Button'
@@ -14,62 +18,36 @@ import {
   Price,
 } from './styled'
 
-class Detail extends Component {
-  state = {
-    product: null,
-  }
+const Detail = ({ match, addProduct }) => {
+  const { productId } = match.params
+  const { data: product } = useApi(() => getProductById(productId), [productId])
 
-  componentDidMount() {
-    const { productId } = this.props.match.params
-    this.fetchProduct(productId)
-  }
-
-  componentDidUpdate(prevProps) {
-    const { productId } = this.props.match.params
-    if (prevProps.match.params.productId !== productId) {
-      this.fetchProduct(productId)
-    }
-  }
-
-  fetchProduct = async productId => {
-    const product = await getProductById(productId)
-    this.setState({ product })
-  }
-
-  render() {
-    const { product } = this.state
-    const { dispatch } = this.props
-
-    return (
-      <Layout>
-        <Wrapper>
-          {product && (
-            <>
-              <ImgWrapper>
-                <Img src={product.image_url} />
-              </ImgWrapper>
-              <DetailsWrapper>
-                <H1 textAlign="center">{product.name}</H1>
-                <Price>{product.price.formatted_amount}</Price>
-                <Description>{product.description}</Description>
-                <Button
-                  onClick={() =>
-                    dispatch({
-                      type: 'cartItems/ADD',
-                      payload: product.id,
-                    })
-                  }
-                >
-                  Add to Cart
-                </Button>
-                <Link to="/">Back</Link>
-              </DetailsWrapper>
-            </>
-          )}
-        </Wrapper>
-      </Layout>
-    )
-  }
+  return (
+    <Layout>
+      <Wrapper>
+        {product && (
+          <>
+            <ImgWrapper>
+              <Img src={product.image_url} />
+            </ImgWrapper>
+            <DetailsWrapper>
+              <H1 textAlign="center">{product.name}</H1>
+              <Price>{product.price.formatted_amount}</Price>
+              <Description>{product.description}</Description>
+              <Button onClick={() => addProduct(product.id)}>
+                Add to Cart
+              </Button>
+              <Link to="/">Back</Link>
+            </DetailsWrapper>
+          </>
+        )}
+      </Wrapper>
+    </Layout>
+  )
 }
 
-export const ProductDetail = connect()(Detail)
+const mapDispatchToProps = {
+  addProduct: addProductAction,
+}
+
+export const ProductDetail = connect(null, mapDispatchToProps)(Detail)
